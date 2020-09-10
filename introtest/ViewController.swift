b9a5aa11dcbcff7771669dda2e30bc3dfff78c6a
Joel Thedéen <joelthedeen@gmail.com>
2020-09-10 09:18:39 +0200

    new layout

---
+++ introtest/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  introtest
+//
+//  Created by Joel Thedéen on 2020-09-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var Label1: UILabel!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
+    }
+
+    @IBAction func changebuttoncolor(_ sender: Any) {
+        
+        
+        Label1.text = "Klickat på knapp"
+        
+    
+        
+    }
+    
+}
+
